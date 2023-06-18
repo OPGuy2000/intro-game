42c3b2ebc8df227facc89a4deea2766ea501c7c9
Prakhar Thapliyal <thapliyal.prakhar@gmail.com>
2023-06-18 00:12:58 -0400

    Added trails and dashing

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -5,18 +5,46 @@ using UnityEngine;
 public class MoveScript : MonoBehaviour  
 {
     public CharacterController controller;
+    public TrailRenderer trailrenderer;
     public float moveSpeed;
 
+    public float dashAmplifier = 20f;
+    private float dashTime = 0.25f;
+    private float dashCooldown = 3f;
+    private bool dashAvailaible = true;
+
+    private Vector3 direction;
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (!controller.isGrounded)
+            direction.y = -0.5f;
 
         if (direction.magnitude >= 0.1f) {
             controller.Move(direction * moveSpeed * Time.deltaTime);
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashAvailaible) 
+            StartCoroutine(Dash());
+    }
+
+    IEnumerator Dash() {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + dashTime) {
+            dashAvailaible = false;
+            trailrenderer.emitting = true;
+            controller.Move(direction*dashAmplifier*Time.deltaTime);
+            yield return null;
+        }
+        trailrenderer.emitting = false;
+        
+        yield return new WaitForSeconds(dashCooldown);
+        dashAvailaible = true;
     }
 }
